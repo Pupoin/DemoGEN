ae1bcd6d1860658a609e48244631df9e38ad3930
Qiang <qiang.li@cern.ch>
2018-08-21 11:13:42 +0200

    add more features

--- plugins/DemoAnalyzer.cc
+++ plugins/DemoAnalyzer.cc
@@ -72,6 +72,7 @@ class DemoAnalyzer : public edm::one::EDAnalyzer<edm::one::SharedResources>  {
       const double ptMax;
       TH1D *h1_w;
       TTree* outTree_;
+      double Htot, Hbb, Hgg, Hww, Hzz;
 };
 
 //
@@ -99,6 +100,7 @@ ptMax(iConfig.getParameter<double>("PtMax"))
    h1_w = fs->make<TH1D>("h1_w", "w", ptNBins, ptMin, ptMax);
    outTree_ = fs->make<TTree>("ntree","ntree");
    outTree_->Branch("pweight"        , pweight       ,"pweight[1000]/D");
+   Htot=0.; Hbb=0.; Hgg=0.; Hww=0.; Hzz=0.;
 }
 
 
@@ -129,18 +131,38 @@ DemoAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
    edm::Handle<edm::View<reco::GenParticle> > genParticles;//define genParticle
    iEvent.getByToken(genSrc_, genParticles);
 
-
+   std::cout<<"--------begin-------------"<<std::endl;
+   Htot++;
    for(size_t ik=0; ik<genParticles->size();ik++) 
-   {// loop on gen
-        
-     const reco::Candidate* ptop = &(*genParticles)[ik];
-     if(abs(ptop->pdgId())== 11 || abs(ptop->pdgId())== 13) {
+   {//1_loop on gen
+     const reco::Candidate* ph = &(*genParticles)[ik];
+   
+     if(abs(ph->pdgId())== 25) {//2_loop H
         std::cout<<"ik= "<<ik<<std::endl; 
-        std::cout<<"pdgId= "<<ptop->pdgId()<<" status="<<ptop->status()<<std::endl;
-        std::cout<<"fromHardProcessFinalState= "<<(*genParticles)[ik].fromHardProcessFinalState()<<std::endl;
-        std::cout<<"fromHardProcessBeforeFSR= "<<(*genParticles)[ik].fromHardProcessBeforeFSR()<<std::endl;
-     }
-    }//end loop gen 
+        std::cout<<"H_pId= "<<ph->pdgId()<<"status="<<ph->status()<<std::endl;
+        for(int i=0;ph->daughter(i)!=NULL;i++)
+        {//3_loop on Higgs daughter
+          const reco::Candidate* pw = ph->daughter(i);
+          std::cout<<" i= "<<i<<std::endl;
+          std::cout<<" W_pId= "<<pw->pdgId()<<"mass="<<pw->mass()<<"status="<<pw->status()<<std::endl;
+
+          if((pw->pdgId())==24) { Hww++; }
+          if((pw->pdgId())==5) { Hbb++; }
+          if((pw->pdgId())==21) { Hgg=Hgg+0.5; }
+          if((pw->pdgId())==23) { Hzz=Hzz+0.5; }
+
+          if(abs(pw->pdgId())==24) {//4_loop on W daughter          
+             for(int j=0;pw->daughter(j)!=NULL;j++) {
+             const reco::Candidate* pl = pw->daughter(j);
+             std::cout<<"   j= "<<j<<std::endl;
+             std::cout<<"   pId= "<<pl->pdgId()<<"status="<<pl->status()<<std::endl;
+             }   
+          } //4_end loop on W daughter
+         }//3_end loop on Higgs daugther
+        }//2_end H
+//        std::cout<<"fromHardProcessFinalState= "<<(*genParticles)[ik].fromHardProcessFinalState()<<std::endl;
+//        std::cout<<"fromHardProcessBeforeFSR= "<<(*genParticles)[ik].fromHardProcessBeforeFSR()<<std::endl;
+    }//1_end loop gen 
 
 
    edm::Handle<LHEEventProduct> wgtsource;
@@ -150,6 +172,7 @@ DemoAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
       //std::cout<<i<<" "<<pweight[i]<<std::endl;
    }
    outTree_->Fill();
+   std::cout<<"Htot:Hgg:Hbb:Hww:Hzz"<<Htot<<" "<<Hgg<<" "<<Hbb<<" "<<Hww<<" "<<Hzz<<std::endl;
 }
 
 
